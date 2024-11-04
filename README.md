c7a686d97bcebbe15b4442dcd69bf19b637c35fd
Florencia Adaro <flor.adaro@gmail.com>
2024-11-04 03:45:40 -0300

    change mode responsive

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Solution URL: [GitHub](https://floradaro.github.io/faq-accordion-solution/)
 
 ## My process
 
--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@ const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach((item) => {
   const question = item.querySelector(".faq-question");
-  const icon = item.querySelector(".faq-icon img"); // Seleccionar la imagen del icono
+  const icon = item.querySelector(".faq-icon img");
 
   question.addEventListener("click", () => {
     item.classList.toggle("active");
 
-    // Alternar las imágenes entre plus y minus
     if (item.classList.contains("active")) {
-      icon.src = "assets/images/icon-minus.svg"; // Cambia a la imagen de menos
+      icon.src = "assets/images/icon-minus.svg";
     } else {
-      icon.src = "assets/images/icon-plus.svg"; // Cambia de vuelta a la imagen de más
+      icon.src = "assets/images/icon-plus.svg";
     }
 
-    // Opcional: Cierra los otros items abiertos
     faqItems.forEach((i) => {
       if (i !== item) {
         i.classList.remove("active");
-        i.querySelector(".faq-icon img").src = "assets/images/icon-plus.svg"; // Restablece el ícono de plus
+        i.querySelector(".faq-icon img").src = "assets/images/icon-plus.svg";
       }
     });
   });
